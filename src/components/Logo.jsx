5a4bd550d13e80d8d5ba68e7ea95ef4221472a6b
Zahidulla <88815355+Zahidulla@users.noreply.github.com>
2025-05-22 21:06:49 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <a href="#home" className="flex items-center">
       <img
-        src="src\components\Option 2 Aligned Logo Png.png" // Make sure logo.png is placed inside the public folder
+        src="https://images.pexels.com/photos/32205933/pexels-photo-32205933.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" // Make sure logo.png is placed inside the public folder
         alt="Emphasis Designs Logo"
         className="w-30 h-14 object-contain" // Increased size
       />
