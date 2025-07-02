1c9a06b9aa49f0b262007ddc62552924a6c8851e
Zahidulla <88815355+Zahidulla@users.noreply.github.com>
2025-07-02 13:09:42 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <a href="#home" className="flex items-center">
       <img
-        src="https://images.pexels.com/photos/32235130/pexels-photo-32235130.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" // Make sure logo.png is placed inside the public folder
+        src="https://res.cloudinary.com/dgztlk3y1/image/upload/v1751441885/WhatsApp_Image_2025-06-01_at_6.09.50_PM_1_hixsag.png" // Make sure logo.png is placed inside the public folder
         alt="Emphasis Designs Logo"
         className="w-30 h-14 object-contain" // Increased size
       />
